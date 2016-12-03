a7a54bc0bf3589c242db145b10d3fe6ca49eebbc
sujayv <sujay.vaishampayan@gmail.com>
2016-12-02 20:11:15 -0700

    Latest Update to Working Code

--- CSE512_Phase3/src/GISCup.java
+++ CSE512_Phase3/src/GISCup.java
@@ -1,23 +1,11 @@
 import java.io.*;
-import java.math.RoundingMode;
-import java.net.URL;
-import java.text.DecimalFormat;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Map.Entry;
 
-import org.apache.hadoop.fs.Path;
 import org.apache.spark.api.java.*;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFunction;
-
-import com.univocity.parsers.common.*;
-import com.univocity.parsers.common.processor.*;
-import com.univocity.parsers.common.record.*;
-import com.univocity.parsers.conversions.*;
-import com.univocity.parsers.csv.*;
 import java.util.*;
-
-import org.apache.spark.SparkConf;
 
 import scala.Tuple2;
 public class GISCup implements Serializable{
@@ -26,22 +14,18 @@ public class GISCup implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private JavaRDD<ArrayList<String>> fetchedrecords = null;
-	private ArrayList<String> header = null; 
 	private JavaSparkContext sc;
+	private JavaRDD<ArrayList<String>> records = null;
 	
-	public GISCup(JavaSparkContext sc)
+	public GISCup(JavaSparkContext sc, String inputFile, String outputFile) throws FileNotFoundException, UnsupportedEncodingException
 	{
 		this.sc = sc;
+		this.readFile(inputFile);
+		this.sortRecords(outputFile);
+		//this.results(outputFile);
 	}
 	
 	private static Function mapfunc = new Function<String, ArrayList<String>>() {
-		
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
 
 		@Override
     	public ArrayList<String> call(String line) throws Exception {
@@ -55,24 +39,9 @@ public class GISCup implements Serializable{
     };
 	
 	
-	public void readFile()
+	public void readFile(String inputFile)
 	{
-	    JavaRDD<ArrayList<String>> records = sc.textFile("hdfs://master:54310/yellow_tripdata_2015-01_shortened.csv").map(mapfunc);
-	    System.out.println("Done loading csv file");
-	    //System.out.println("The number of lines are"+records.count());
-	    System.out.println("Press enter");
-	    fetchedrecords = records.cache();
-	    /*for(int i=0;i<fetchedrecords.count();i+=100000)
-	    {
-	    	for(ArrayList<String> tp : fetchedrecords.take(i))
-    		{
-	    		for(int j=0;j<tp.size();j++)
-	    		{
-	    			System.out.print(tp.get(j)+"--");
-	    		}
-	    		System.out.println();
-    		}
-	    }*/
+	    records = sc.textFile(inputFile).map(mapfunc);
 	}
 	
 	double X_avg = 0.0;
@@ -80,31 +49,27 @@ public class GISCup implements Serializable{
 	double n = 55 * 40 * 31;
 	double X_bar = 0.0;
 	double S = 0.0;
-	public void sortRecords()
+	public void sortRecords(String outputFile) throws FileNotFoundException, UnsupportedEncodingException
 	{
-		//header = fetchedrecords.first();
-		JavaRDD<ArrayList<String>> resultRows = fetchedrecords;
-		JavaPairRDD<ArrayList<Integer>, ArrayList<String>> mapped_data = resultRows.mapToPair(transform);
-		JavaPairRDD<ArrayList<Integer>, ArrayList<String>> filtered_data = mapped_data.filter(filterNullVal);
-		JavaPairRDD<ArrayList<Integer>, Iterable<ArrayList<String>>> grouped_data = filtered_data.groupByKey();
-		JavaPairRDD<ArrayList<Integer>, Integer> attribute_data = grouped_data.mapToPair(calculateAttribute);
-		List<Tuple2<ArrayList<Integer>, Integer>> list = attribute_data.collect();
-		HashMap<ArrayList<Integer>,Integer> keyvalue = new HashMap<>();
-		for(Tuple2<ArrayList<Integer>, Integer> row : list)
+		JavaPairRDD<String, Integer> mapped_data = records.mapToPair(normalize);
+		JavaPairRDD<String, Integer> filtered_data = mapped_data.filter(filterNullVal);
+		//System.out.println("Count of filtered_data is : "+filtered_data.count());
+		Map<String, Integer> grouped_data1 = filtered_data.reduceByKeyLocally(reducingMethod);
+		HashMap<String,Integer> grouped_data = new HashMap<>(grouped_data1);
+		//System.out.println("Count of grouped_data is : "+grouped_data.size());
+		for(Entry<String,Integer> entry : grouped_data.entrySet())
 		{
-			keyvalue.put(row._1,row._2);
+			int temp = entry.getValue();
+			X_avg += temp;
+			X_avg_square += temp * temp;
 		}
-		long attributesize = attribute_data.count();
-		for(Tuple2<ArrayList<Integer>, Integer> temp : attribute_data.take((int)attributesize))
-		{
-			X_avg += temp._2;
-			X_avg_square += X_avg * X_avg;
-		}
-		X_bar = X_avg / n;
+		X_bar = (double) X_avg / n;
 		S = Math.sqrt((X_avg_square / n) - (X_bar * X_bar));
-		TreeMap<Double,ArrayList<Integer>> zscore = new TreeMap<>();
-		for(Tuple2<ArrayList<Integer>,Integer> tp: list)
+		TreeMap<Double,String> zscore = new TreeMap<>();
+		int a=0,b=0,c=0,d=0;
+		for(Entry<String,Integer> entry : grouped_data.entrySet())
 		{
+			String key = entry.getKey();
 			double Gstar = 0.0;
 			double sum = 0.0;
 			int count = 0;
@@ -114,21 +79,19 @@ public class GISCup implements Serializable{
 				{
 					for(int k=-1;k<2;k++)
 					{
-						ArrayList<Integer> tp1 = new ArrayList<>();
-						tp1.add(tp._1.get(0)+i);
-						tp1.add(tp._1.get(1)+j);
-						tp1.add(tp._1.get(2)+k);
-						if(keyvalue.containsKey(tp1))
+						String parts[] = key.split(",");
+						int newdate = Integer.parseInt(parts[0]) + i;
+						int newlat = Integer.parseInt(parts[1]) + j;
+						int newlong = Integer.parseInt(parts[2]) + k;
+						String newkey = newdate + "," + newlat + "," + newlong;
+						if(grouped_data.containsKey(newkey))
 						{
-							sum += tp._2;
+							sum += (int)grouped_data.get(newkey);
 							count++;
 						}
 						else
 						{
-							int timestep = tp._1.get(0) + i;
-							int longitude = tp._1.get(1) + j;
-							int latitude = tp._1.get(2) + k;
-							if(!(timestep < 1 && timestep > 31 && latitude < 0 && latitude > 40 && longitude < 0 && longitude > 55))
+							if(!((newdate < 1 || newdate > 31) || (newlat < 4050 || newlat > 4090) || (newlong < 7370 || newlong > 7425)))
 							{
 								count++;
 							}
@@ -136,96 +99,112 @@ public class GISCup implements Serializable{
 					}
 				}
 			}
+			if(count == 27)
+			{
+				a++;		
+			}
+			else if(count == 18)
+			{
+				b++;		
+			}
+			else if(count == 12)
+			{
+				c++;		
+			}
+			else if(count == 8)
+			{
+				d++;		
+			}
 			double num = sum - X_bar * count;
 			double den = S * Math.sqrt((n * count - count * count) / (n-1));
 			Gstar = num / den;
-			zscore.put(Gstar, tp._1);
+			zscore.put(Gstar, key);
 		}
-		Map<Double,ArrayList<Integer>> sortedzscore = zscore.descendingMap();
-		for(Double value : sortedzscore.keySet())
+		//System.out.println("The number of 27 neighbored cells are: "+a);
+		//System.out.println("The number of 18 neighbored cells are: "+b);
+		//System.out.println("The number of 12 neighbored cells are: "+c);
+		//System.out.println("The number of 8 neighbored cells are: "+d);
+		Map<Double,String> sortedzscore = zscore.descendingMap();
+		int ct = 0;
+		PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+		Set<Double> sortedkeys = sortedzscore.keySet();
+		for(Double value : sortedkeys)
 		{
+			if(++ct == 51)
+			{
+				break;
+			}
+			String temporary = sortedzscore.get(value);
+			String temp[] = temporary.split(",");
+			int date = Integer.parseInt(temp[0]);
+			int latitude = Integer.parseInt(temp[1]);
+			int longitude = Integer.parseInt(temp[2]);
 			System.out.print("Key: ");
-			ArrayList<Integer> temporary = sortedzscore.get(value);
-			System.out.println(temporary.get(0)+"--"+temporary.get(1)+"--"+temporary.get(2));
+			System.out.println(date+"--"+latitude+"--"+longitude);
 			System.out.println("Value: "+value);
 			System.out.println("******************");
-			
+			double final_latitude = latitude / 100.00;
+			double final_longitude = longitude / 100.00;
+			writer.print(final_latitude+","+(0-final_longitude)+","+date+","+value+"\n");
 		}
+		writer.close();
 		/*for(Tuple2<ArrayList<Integer>, Integer> temp : attribute_data.take(1000))
 	    {
 	    	System.out.println(temp._1.get(0) + "--"+ temp._1.get(1) + "--"+ temp._1().get(2));
 	    	System.out.println("Size of the cell is "+temp._2);
 	    	
-	    }*/	
-	}
+	    }*/			
+	}    
+    
+    
+	static Function2 reducingMethod = new Function2<Integer, Integer, Integer>() {
+
+		@Override
+		public Integer call(Integer arg0, Integer arg1) {
+			// TODO Auto-generated method stub
+			return arg0 + arg1;
+		}
+		
+	};
 	
 	
-    
-    // Filter Header
-    /*static Function<ArrayList<String>, Boolean> filterFirstRow = new Function<ArrayList<String>, Boolean>() {
-    	public Boolean call(ArrayList<String> line) {
-    		      return (!line.get(1).equalsIgnoreCase(header.get(1)));
-    	}
-    };*/
-    
-    
-    
-    static PairFunction<ArrayList<String>, ArrayList<Integer>, ArrayList<String>> transform = new PairFunction<ArrayList<String> , ArrayList<Integer>, ArrayList<String>>() {
-		  public Tuple2<ArrayList<Integer>, ArrayList<String>> call(ArrayList<String> row) {
-			    int timeStep, longitude_cell, latitude_cell;
-			    Double longitude_conv, latitude_conv;
-			    ArrayList<Integer> transformed_row = new ArrayList<Integer>();
+	
+    static PairFunction<ArrayList<String>, String, Integer> normalize = new PairFunction<ArrayList<String> , String, Integer>() {
+		  public Tuple2<String, Integer> call(ArrayList<String> row) {
+			    int date, longitude_cell, latitude_cell;
+			    double longitude_conv, latitude_conv;
+			    String transformed_row = null;
 			  
-				//TimeStep
-				timeStep = Integer.parseInt(row.get(0).split(" ")[0].split("-")[2]);
+				//date
+				date = Integer.parseInt(row.get(0).split(" ")[0].split("-")[2]);
 				
 				//Longitude cell
-				longitude_conv =  Math.floor ((Double.parseDouble(row.get(1)) * 100.0)) / 100.0;
-				longitude_cell = (int)((longitude_conv + 74.25)/0.01);
+				longitude_conv =  Math.ceil ((Double.parseDouble(row.get(1)) * 100.0));
+				longitude_cell = Math.abs((int)longitude_conv);
 				
 				//Latitude cell
-				latitude_conv =  Math.floor ((Double.parseDouble(row.get(2)) * 100.0)) / 100.0;
-				latitude_cell = (int)((latitude_conv - 40.50)/0.01);
+				latitude_conv =  Math.floor ((Double.parseDouble(row.get(2)) * 100.0));
+				latitude_cell = Math.abs((int)latitude_conv);
 				
 				
-				if (timeStep < 1 || longitude_cell < 0 || longitude_cell > 55 ||
-						latitude_cell < 0 || latitude_cell > 40)
+				if (date < 1 || date > 31 || longitude_cell < 7370 || longitude_cell > 7425 ||
+						latitude_cell < 4050 || latitude_cell > 4090)
 						return null;
 			
 				else{
-					transformed_row.add(timeStep);
-					transformed_row.add(longitude_cell);
-					transformed_row.add(latitude_cell);
+					transformed_row = date + "," + latitude_cell + "," + longitude_cell;
 
-					return new Tuple2(transformed_row, row);
+					return new Tuple2(new String(transformed_row), 1);
 				}
 		  }
 	};
 		
 	
     // Filter null transformed row
-	static Function<Tuple2<ArrayList<Integer>, ArrayList<String>>, Boolean> filterNullVal = new Function<Tuple2<ArrayList<Integer>, ArrayList<String>>, Boolean>() {
-		public Boolean call(Tuple2<ArrayList<Integer>, ArrayList<String>> row) throws Exception {
+	static Function<Tuple2<String, Integer>, Boolean> filterNullVal = new Function<Tuple2<String, Integer>, Boolean>() {
+		public Boolean call(Tuple2<String, Integer> row) throws Exception {
 	    	   	return row != null;
 	    	   }
     };
-    
-    static PairFunction calculateAttribute = new PairFunction<Tuple2<ArrayList<Integer>, Iterable<ArrayList<String>>>,ArrayList<Integer>, Integer>() {
-
-		@Override
-		public Tuple2<ArrayList<Integer>, Integer> call(Tuple2<ArrayList<Integer>, Iterable<ArrayList<String>>> row)throws Exception 
-		{
-	    	Iterator<ArrayList<String>> it = row._2.iterator();
-	    	int count = 0;
-	    	while(it.hasNext())
-	    	{
-	    		it.next();
-	    		count += 1;
-	    	}
-	    	return new Tuple2(row._1,count);
-		}
-    };
-    
-
 }
 
